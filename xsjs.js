0b0b1b5aaaa96b7062d3e7e2a02f8710497dfe7e
caglarorhan <caglaror@gmail.com>
2024-04-05 00:14:51 -0500

    feature: parse and manipulate properties of an element- first try

--- xsjs.js
+++ xsjs.js
@@ -1,10 +1,31 @@
 const xsJS ={
+    xs: null,
     init(){
-        document.querySelectorAll("[xsjs]").forEach(item=>{
-            console.log(item);
+        this.xs = document.querySelectorAll("[xsjs]");
+        this.extend();
+    },
+    extend(){
+        this.xs.forEach(el=>{
+            console.log(el.tagName);
+            if(el.hasAttribute("do")){
+                console.log(el.getAttribute("do"));
+                this.parseExtend({src:el, data: el.getAttribute("do")});
+            }
         })
+    },
+    parseExtend(o={src:HTMLElement, data:String}){
+        let parsedData = JSON.parse(o.data);
+        Object.entries(parsedData).forEach(([k,v])=>{
+            o.src[k] = v;
+        })
+    },
+    do(o={what:String, to:HTMLElement, with:Object}){
+
     }
+
 }
+
+
 
 const windowListener = window.addEventListener('load',()=>{
     xsJS.init();
